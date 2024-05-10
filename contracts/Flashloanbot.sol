6ad1a91569b4700f539e1dc0b771831d5d9b1a8e
billiedox <edox.billi@gmail.com>
2024-05-10 22:23:44 +0300

    fixed bug for executing bot

--- contracts/Flashloanbot.sol
+++ contracts/Flashloanbot.sol
@@ -11,7 +11,10 @@ contract Flashloanbot is IFlashLoanRecipient, Ownable {
 
     address public immutable vault;
     IUniswapV2Router02 private router;
-    IERC20 private weth;
+    IERC20 borrowToken;
+    IERC20 swapToken;
+
+    event executeSuccess(uint256 amountBefore, uint256 amountAfter);
 
     constructor(address _vault) {
         vault = _vault;
@@ -23,32 +26,35 @@ contract Flashloanbot is IFlashLoanRecipient, Ownable {
         uint256[] memory,
         bytes memory
     ) external override {
-        IERC20 token = tokens[0];
         uint256 amount = amounts[0];
-        uint256[] memory amountOuts1 = _swapTokens(address(tokens[0]), address(tokens[1]), amount);
-        _swapTokens(address(tokens[1]), address(tokens[0]), amountOuts1[0]);
-        uint currentBal = token.balanceOf(address(this));
-        if (currentBal < amount) {
-            token.transferFrom(msg.sender, address(this), (amount - currentBal));
-        }
-        
-        token.transfer(vault, amount);
+        uint256[] memory amountOuts1 = _swapTokens(address(borrowToken), address(swapToken), amount);
+        uint256[] memory amountOuts2 = _swapTokens(address(swapToken), address(borrowToken), amountOuts1[0]);
+        uint currentBal = borrowToken.balanceOf(address(this));
+
+        require(currentBal >= amount, "No profit");
+
+        borrowToken.transfer(vault, amount);
+        emit executeSuccess(amount, amountOuts2[0]);
     }
 
     function execute(
-        IERC20[] memory _tokens,
-        uint256[] memory _amounts,
+        address _borrowToken,
+        address _swapToken,
+        uint256 _amount,
         address _router,
-        address _weth,
         bytes memory userData
     ) external {
-        require(_tokens.length == 2, "Invalid number of tokens");
+        require(_amount > 0, "Invalid borrow amount");
+        require(_borrowToken != address(0) && _swapToken != address(0), "Invalid token address");
+        borrowToken = IERC20(_borrowToken);
+        swapToken = IERC20(_swapToken);
         router = IUniswapV2Router02(_router);
-        weth = IERC20(_weth);
         IERC20[] memory tokens;
-        tokens[0] = _tokens[0];
+        tokens = new IERC20[](1);
+        tokens[0] = IERC20(_borrowToken);
         uint256[] memory amounts;
-        amounts[0] = _amounts[0];
+        amounts = new uint256[](1);
+        amounts[0] = _amount;
         IBalancerVault(vault).flashLoan(
             IFlashLoanRecipient(address(this)),
             tokens,
@@ -68,17 +74,8 @@ contract Flashloanbot is IFlashLoanRecipient, Ownable {
         path[0] = tokenA;
         path[1] = tokenB;
         uint256[] memory amountOuts = router.swapExactTokensForTokens(
-            amount, 0, path, address(this), block.timestamp
+            amount, 0, path, address(this), block.timestamp + 3600
         );
         return amountOuts;
-    }
-
-    function withdraw(address _tokenAddr) external onlyOwner {
-        IERC20 token = IERC20(_tokenAddr);
-        uint256 balance = token.balanceOf(address(this));
-        if (balance > 0) {
-            token.approve(msg.sender, balance);
-            token.transfer(msg.sender, balance);
-        }
     }
 }
